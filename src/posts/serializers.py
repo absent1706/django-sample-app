ca2541c36d5b2751cc66705b6b32d8dcd4e9977d
Alex Lytvynenko <oleksandr.lytvynenko@codeit.pro>
2023-03-06 10:39:49 +0200

    ADD added Posts CRUD API

---
+++ src/posts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author_name = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'author', 'author_name', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'author', 'created_at', 'updated_at', 'author_name']
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
