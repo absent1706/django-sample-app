445fdeacab6062a765913203e27b711649c80cb2
Alex Lytvynenko <oleksandr.lytvynenko@codeit.pro>
2023-03-06 14:32:19 +0200

    ADD added Post model to Django admin dash

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(ModelAdmin):
+    pass
