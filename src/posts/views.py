328814126b3aafa5a30b395d9153b6d2d7465ab7
Alex Lytvynenko <oleksandr.lytvynenko@codeit.pro>
2023-03-06 15:02:54 +0200

    ADD added ordering, e.g. '/posts/?ordering=title' or '/posts/?ordering=author__username'

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -10,9 +10,10 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['category']
     search_fields = ['title', 'content']
+    ordering_fields = ['title', 'author__username']
 
     def get_queryset(self):
         return Post.objects.prefetch_related('author').all()
