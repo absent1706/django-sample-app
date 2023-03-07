5eb5da80da4f65de4153a4ca46badede4452ea0a
Alex Lytvynenko <oleksandr.lytvynenko@codeit.pro>
2023-03-07 15:31:46 +0200

    ADD added permissions check for post PUT/PATCH API

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,15 +1,16 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthorOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['category']
     search_fields = ['title', 'content']
